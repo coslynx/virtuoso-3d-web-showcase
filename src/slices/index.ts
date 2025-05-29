3fa2330e48aae2fc8192250776c13c3521e7d357
CosLynx AI <drixisgod@gmail.com>
2025-05-29 18:02:31 +0530

    added template file: `src/slices/index.ts`

---
+++ src/slices/index.ts
@@ -0,0 +1,4 @@
+export { default as Hero } from './Hero';
+export { default as Carousel } from './Carousel';
+export { default as ScrollExperience } from './ScrollExperience';
+export { default as Contact } from './Contact';
